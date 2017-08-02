a88b2a2d464ddf8d5a2e034d9f26b8acdb6e19f7
AustinYeon <umovito@gmail.com>
2017-08-03 03:02:55 +0900

    connect

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-$.ajax({
-  method: "GET",
-  beforeSend: function(request) {
-    request.setRequestHeader("X-Mashape-Key", 'JffA8g8glAmsh2NPH79UZNpC40xbp1V4Y2njsnOsAsnikSS5x2');
-  },
-  url: "https://omgvamp-hearthstone-v1.p.mashape.com/cards/Ysera",
-  dataType: 'json',
-  success: function(data) {
-    data = JSON.parse(data);
-    console.log(data);
-  }
-})
+$(function() {
+  $("#button").click(function(e){
+    e.preventDefault();
+    $.ajax({
+      method: "GET",
+      beforeSend: function(request) {
+        request.setRequestHeader("X-Mashape-Key", 'JffA8g8glAmsh2NPH79UZNpC40xbp1V4Y2njsnOsAsnikSS5x2');
+      },
+      url: "https://omgvamp-hearthstone-v1.p.mashape.com/cards/Ysera",
+      dataType: 'json',
+      success: function(data) {
+        data = JSON.parse(data);
+        console.log(data);
+      }
+    });
+  });
+});
