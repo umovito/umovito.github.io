bafc2b14e572c1d778e3f54a3736ced6c049a313
AustinYeon <umovito@gmail.com>
2017-08-03 18:29:02 +0900

    random basic cards

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 $(function() {
-  $("#button").click(function(e){
-    e.preventDefault();
-    $.ajax({
-      method: "GET",
-      beforeSend: function(request) {
-        request.setRequestHeader("X-Mashape-Key", 'JffA8g8glAmsh2NPH79UZNpC40xbp1V4Y2njsnOsAsnikSS5x2');
-      },
-      url: "https://omgvamp-hearthstone-v1.p.mashape.com/cards/Ysera",
-      dataType: 'json',
-      success: function(data) {
-        data = JSON.parse(data);
-        console.log(data);
-        $("#img").attr('src', data[0]['imgGold']);
-      }
-    });
-  });
-});
+        var basiccards = ['개구리', '기습', '넘치는 마나', '달빛섬광', '동전 한 닢', '동전의 화신', '선인의 치유력', '정신 자극', '토템의 힘', '환영 복제', '희생의 서약', '겸손', '공허방랑자', '냉기 충격', '돌발톱 토템', '돌엄니멧돼지', '돌진', '마음의 눈', '맹독', '멀록 약탈꾼', '멀록 정찰병', '멧돼지', '보호의 손길', '부두교 의술사', '부패', '북녘골 성직자', '불타는 토템', '사냥꾼의 징표', '사악한 일격', '성난비늘 수련사', '성스러운 일격', '소용돌이', '소형 기계용', '신비한 사격', '신비한 화살', '신의 권능: 보호막', '양', '엘프 궁수', '영혼의 불꽃', '은빛 성기사단 신병', '죽음의 고리', '천벌의 토템', '추적', '치유 토템', '할퀴기', '해골', '환영 복제', '황금골 보병', '회갈색 늑대', '힘의 축복', '급속 성장', '대지의 무기', '독칼', '마무리 일격', '멀록 바다사냥꾼', '민물악어', '불꽃의 토템', '붉은늪지랩터', '산성 늪수액괴물', '서리늑대 그런트', '서큐버스', '성스러운 빛', '신비한 폭발', '안 돼애애애애애', '야생의 징표', '어둠의 권능: 고통', '얼음 화살', '영웅의 일격', '정신 분열', '질풍', '천상의 정신', '코볼트 흙점쟁이', '푸른아가미 전사', '풋내기 기술자', '혼절시키기', '회전베기', '가시덩굴 사냥꾼', '공격대장', '늑대기수', '달라란 마법사', '레오크', '무너진 태양 성직자', '무쇠가죽 불곰', '미샤', '방패 막기', '사술', '살상 명령', '생명력 흡수', '신비한 지능', '아이언포지 소총병', '야생의 벗', '야생의 포효', '어둠의 권능: 죽음', '어둠의 화살', '얼음 회오리', '용암 광전사', '은빛털고릴라 우두머리', '전쟁노래 사령관', '치유의 손길', '칼날 부채', '킁킁이', '기계용 정비사', '노움 발명가', '물의 정령', '바람예언자', '변이', '사냥개조련사', '서리바람 설인', '센진 방패대가', '스톰윈드 기사', '신성화', '오아시스 무쇠턱거북', '오우거 마법사', '왕의 축복', '일제 사격', '지옥의 불길', '천벌의 망치', '코르크론 정예병', '화염구', '휘둘러치기', '구루바시 광전사', '굶주린 대머리수리', '무법항 경호원', '서리늑대 전쟁군주', '스톰파이크 특공대', '신성한 폭발', '암살', '암흑칼날', '어둠비늘 치유사', '툰드라 코뿔소', '피의 욕망', '공포의 지옥불정령', '대마법사', '돌주먹 오우거', '못 말리는 로켓병', '별빛섬광', '불의 정령', '소멸', '투기장의 제왕', '불기둥', '스톰윈드 용사', '심장부 사냥개', '왕의 수호자', '전력 질주', '전쟁 골렘', '무쇠껍질 수호정령', '정신 지배'];
+        $("#button").click(function(e){
+          e.preventDefault();
+          var requrl = 'https://omgvamp-hearthstone-v1.p.mashape.com/cards/' + basiccards[Math.floor(Math.random()*basiccards.length)];
+          requrl += '?locale=koKR';
+          console.log(requrl);
+          $.ajax({
+            method: "GET",
+            beforeSend: function(request) {
+              request.setRequestHeader("X-Mashape-Key", 'JffA8g8glAmsh2NPH79UZNpC40xbp1V4Y2njsnOsAsnikSS5x2');
+            },
+            url: requrl,
+            dataType: 'json',
+            success: function(data) {
+              $("#img").attr('src', data[0]['imgGold'] || data[1]['imgGold']);
+            }
+          });
+        });
+      });
